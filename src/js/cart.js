b50d387b838d7abcbe0b5ca9783e6c95ff78124a
Austin Earl <austinearl78@gmail.com>
2025-05-11 16:32:07 -0400

    more stuff

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,31 @@
+var order = document.getElementById('order');
+parent_div = document.createElement("food-options");
+
 window.addEventListener("DOMContentLoaded", () => {
     let str_food = localStorage.getItem("order");
     let food_items = JSON.parse(str_food);
     food_items.forEach(element => {
-        console.log(element);
+        let order_item = document.createElement('div');
+        order_item.classList.add('food-option');
+        let img = document.createElement('img');
+        img.classList.add('food-img');
+        img.src = getImage(element);
+        img.alt = element;
+        order_item.innerText = element;
+
+        order_item.appendChild(img);
+        order.appendChild(order_item);
     });
 })
+
+
+function getImage(text) {
+    image_path = "";
+    switch (text) {
+        case "Burger":
+            image_path = "../../images/Foods/burger.jpg";
+            break;
+    }
+
+    return image_path;
+}
