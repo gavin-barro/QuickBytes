6f2243740215259e1b439190c167374d9b2dad3f
Austin Earl <austinearl78@gmail.com>
2025-05-12 15:31:05 -0400

    stuff

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,31 +1,38 @@
-var order = document.getElementById('order');
-parent_div = document.createElement("food-options");
+var order = document.getElementById('cart-items');
+var total_price = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
-    let str_food = localStorage.getItem("order");
-    let food_items = JSON.parse(str_food);
+    food_items = JSON.parse(localStorage.getItem("order"));
     food_items.forEach(element => {
+        item_name = element.name;
+        item_price = parseFloat(element.price);
+        total_price += item_price;
         let order_item = document.createElement('div');
-        order_item.classList.add('food-option');
+        order_item.classList.add('cart-item');
         let img = document.createElement('img');
-        img.classList.add('food-img');
-        img.src = getImage(element);
-        img.alt = element;
-        order_item.innerText = element;
+        img.classList.add('cart-img');
+        img.src = getImage(item_name);
+        img.alt = item_name;
 
         order_item.appendChild(img);
         order.appendChild(order_item);
+
     });
 })
 
-
 function getImage(text) {
-    image_path = "";
+    var image_link = "";
     switch (text) {
-        case "Burger":
-            image_path = "../../images/Foods/burger.jpg";
+        case 'Burger':
+        case 'Cheeseburger':
+            image_link = '../../images/Foods/burger.jpg';
             break;
-    }
-
-    return image_path;
+        case 'Grilled Chicken Sandwich':
+            image_link = '../../images/Foods/grilled_chicken_sandwich.jpg';
+            break;
+        case 'Caesar Salad':
+            image_link = '../../images/Foods/caesar_salad.jpg';
+            break;
+   }
+   return image_link;
 }
--- src/js/order.js
+++ src/js/order.js
@@ -1,20 +1,25 @@
 const foodItems = document.querySelectorAll(".order-btn");
+
 foodItems.forEach(item => {
-    item.addEventListener("click", ()=>{
-        if (!localStorage.getItem("order")) {
-            food_items = []
-            addItem(food_items, item)
-        } else {
-            let str_food = localStorage.getItem("order");
-            let food_items = JSON.parse(str_food);
-            addItem(food_items, item)
+    item.addEventListener("click", () => {
+        // Initialize order and total
+        let food_items = [];
+
+        // Retrieve existing data if it exists
+        if (localStorage.getItem("order")) {
+            food_items = JSON.parse(localStorage.getItem("order"));
         }
+
+
+        addItem(food_items, item);
     });
 });
 
-function addItem(arr, item) {
+function addItem(arr, item, curr_price) {
     const itemName = item.getAttribute("data-food");
-    arr.push(itemName);
-    let str = JSON.stringify(arr);
-    localStorage.setItem("order", str);
+    const itemPrice = parseFloat(item.getAttribute("data-price"));
+
+    arr.push({ name: itemName, price: itemPrice });
+
+    localStorage.setItem("order", JSON.stringify(arr));
 }
